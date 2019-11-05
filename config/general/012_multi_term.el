066171cb9de400531fa719af8593a61c2f17c5a4
Marcus Pemer <marcus@iteego.com>
2019-11-05 17:10:52 +0100

    Lagavulin

--- config/general/012_multi_term.el
+++ config/general/012_multi_term.el
@@ -1,6 +1,18 @@
 (progn
   (ensure-package-installed 'multi-term)
-  (use-package multi-term))
+  (use-package multi-term
+    :config (progn
+	      (setq multi-term-program "zsh")
+	      (add-hook 'term-mode-hook
+			(lambda ()
+			  (setq term-buffer-maximum-size 10000)))
+	      (add-hook 'term-mode-hook
+			(lambda ()
+			  (add-to-list 'term-bind-key-alist '("M-[" . multi-term-prev))
+			  (add-to-list 'term-bind-key-alist '("M-]" . multi-term-next))))
+	      (add-hook 'term-mode-hook
+			(lambda ()
+			  (define-key term-raw-map (kbd "C-y") 'term-paste))))))
 ;;(setf multi-term-program "/home/mpemer/.nvm/versions/node/v9.11.2/bin/closh")
 ;;     :config (progn
 ;; 	      (setf multi-term-program "sh -c /home/mpemer/bin/closh-zero.jar")
