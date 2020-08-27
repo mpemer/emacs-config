558713602a019dea805ebb572da190db7564a44e
Marcus Pemer <marcus@iteego.com>
2020-08-27 11:18:47 +0000

    Korben

--- config/general.el
+++ config/general.el
@@ -26,12 +26,12 @@
 ;;  (ensure-package-installed 'monokai-theme)
 ;;  (use-package monokai-theme))
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#0b0600" :foreground "#F8F8F2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "Bits" :family "Bitstream Vera Sans Mono")))))
+;;(custom-set-faces
+;; ;; custom-set-faces was added by Custom.
+;; ;; If you edit it by hand, you could mess it up, so be careful.
+;; ;; Your init file should contain only one such instance.
+;; ;; If there is more than one, they won't work right.
+;; '(default ((t (:inherit nil :stipple nil :background "#0b0600" :foreground "#F8F8F2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "Bits" :family "Bitstream Vera Sans Mono")))))
 
 ;;(progn
 ;;  (ensure-package-installed 'color-theme-modern)
