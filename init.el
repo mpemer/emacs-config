df992454b928ad533898dfaa97dbfb3357f8d808
Marcus Pemer <marcus@iteego.com>
2022-04-11 16:44:22 +0200

    hal9k

--- init.el
+++ init.el
@@ -125,12 +125,16 @@
  (set-cursor-color "#eebbaa"))
 
 
-(provide '.emacs)
-;;; .emacs ends here
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "#000000" :foreground "#cbccd1" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "Bits" :family "DejaVu Sans Mono"))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#000000" :foreground "#cbccd1" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Monospace"))))
  '(org-level-1 ((t (:inherit outline-1 :extend nil :foreground "#ccbbaa")))))
+
+(set-face-attribute 'default nil :family (if (eq system-type 'darwin) "Menlo" "DejaVu Sans Mono"))
+
+(provide '.emacs)
+;;; .emacs ends here
