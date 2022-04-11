2bfcb2a86075408a7260de2f33f0d1222e9b443a
Marcus Pemer <marcus@iteego.com>
2022-04-11 14:02:11 +0200

    config cleanup

--- config/general/010_ide.el
+++ config/general/010_ide.el
@@ -28,6 +28,7 @@
 (use-package magit
   :config
   (progn
+    (setq magit-diff-use-overlays nil)
     (global-set-key (kbd "C-x g") 'magit-status)
     (global-set-key (kbd "C-x C-g") 'magit-status)))
 
