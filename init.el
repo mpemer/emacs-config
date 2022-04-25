5f3ce6d8889fbf7b625c2e7d42f7a2ce2a9a8fb9
Marcus Pemer <marcus@iteego.com>
2022-04-25 18:25:03 +0200

    zorg

--- init.el
+++ init.el
@@ -158,8 +158,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:background nil))))
- '(outline-1 ((t (:foreground "LightGoldenrod4"))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#000000" :foreground "#cbccd1" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "PfEd" :family "DejaVu Sans Mono"))))
+ '(outline-1 ((t (:foreground "LightGoldenrod3"))))
  '(outline-2 ((t (:foreground "gray50"))))
  '(outline-3 ((t (:foreground "MediumPurple4")))))
 
