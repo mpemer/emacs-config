d4fa929849c8e6b32015cf4399ec788e0167221e
Marcus Pemer <marcus@iteego.com>
2022-05-17 13:06:54 +0200

    hal9k

--- config/general/005_editor.el
+++ config/general/005_editor.el
@@ -57,11 +57,11 @@
 (global-set-key (kbd "S-C-<up>") 'enlarge-window)
 
 ;; Text scaling
-(global-set-key (kbd "C-=") 'text-scale-increase)
-(global-set-key (kbd "C--") 'text-scale-decrease)
-(global-set-key (kbd "C-+") 'font-size-increase)
-(global-set-key (kbd "C-_") 'font-size-decrease)
-(global-set-key (kbd "C-)") 'font-size-default)
+(global-set-key (kbd "S-C-=") 'text-scale-increase)
+(global-set-key (kbd "S-C--") 'text-scale-decrease)
+;;(global-set-key (kbd "C-+") 'font-size-increase)
+;;(global-set-key (kbd "C-_") 'font-size-decrease)
+;;(global-set-key (kbd "C-)") 'font-size-default)
 
 ;; Enable reopening of recent files via C-x C-r
 (recentf-mode 1)
