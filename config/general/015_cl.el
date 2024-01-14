3d3a707edce5462bcd190e2fa4c31063c21f5872
Marcus Pemer <marcus@pemer.com>
2024-01-14 14:19:23 -0500

    Make cl setup more non-roswell tolerant

--- config/general/015_cl.el
+++ config/general/015_cl.el
@@ -12,8 +12,10 @@
 (defvar roswell-helper-file "~/.roswell/helper.el")
 
 ;; Check if the Roswell helper file exists and load it
-(if (file-exists-p roswell-helper-file)
-    (load roswell-helper-file))
+(when (file-exists-p roswell-helper-file)
+    (load roswell-helper-file)
+    (add-to-list 'load-path (roswell-directory "sly"))
+    )
 
 (my/ensure-package-installed 'sly)
 (use-package sly)
@@ -21,15 +23,14 @@
 (my/ensure-package-installed 'sly-named-readtables)
 (use-package sly-named-readtables)
 
-
-(add-to-list 'load-path (roswell-directory "sly"))
+;;(my/ensure-package-installed 'sly-autoloads)
 (require 'sly-autoloads)
 
 (my/ensure-package-installed 'sly-asdf)
 (use-package sly-asdf)
 
 ;;(my/ensure-package-installed 'sly-contribs)
-;;(use-package sly-asdf)
+;;(use-package sly-contribs)
 
 (my/ensure-package-installed 'sly-quicklisp)
 (use-package sly-quicklisp)
@@ -40,7 +41,9 @@
 (setq inferior-lisp-program "ros dynamic-space-size=2048 -L sbcl -l ~/.sbclrc run"
       sly-lisp-implementations '((sbcl ("sbcl" "dynamic-space-size=2048" "-l" "~/.sbclrc"))
                                  (roswell ("ros" "dynamic-space-size=2048" "-L" "sbcl" "-l" "~/.sbclrc" "run")))
-      sly-default-lisp 'roswell)
+      sly-default-lisp (if (file-exists-p roswell-helper-file)
+			   'roswell
+			 'sbcl))
 
 (provide '015_cl)
 ;; 015_cl.el ends here
